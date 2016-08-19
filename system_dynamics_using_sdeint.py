9633f4b6a9f06af5006b9a5628b9e11a17530636
najamxaidi <najam.xaidi@gmail.com>
2016-08-19 13:37:18 +1000

    ipython notebooks for area under the curve

--- system_dynamics_using_sdeint.py
+++ system_dynamics_using_sdeint.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sdeint
 import matplotlib.pyplot as plt
+from scipy.integrate import simps
+from numpy import trapz
 
 
 class SystemDynamicsWithSdeint:
@@ -38,31 +40,31 @@ class SystemDynamicsWithSdeint:
 
         # required for plotting
         self.orbits = []
+        self.orbits_for_pi_ei = []
         for i in range(self.options):
             self.orbits.append([])
+            self.orbits_for_pi_ei.append([])
 
     def rotation_of_utilitities(self):
         self.utility_of_choices = np.roll(self.utility_of_choices,1)
 
-
     def rate_of_experience(self, experience, t):
-        #print(experience)
 
-        # Equation 3.1 starts (this is pi)
-        # calculate the probability based upon the initial experiences
         self.count += 1
         if self.count == self.rotation_time and self.flag == True:
             self.utility_of_choices = np.roll(self.utility_of_choices, 1)
 
-        # calculate probability
+        # Equation 3.1 starts (this is pi)
+        # calculate the probability based upon the initial experiences
         options_probability = (np.power((experience + self.k), self.alpha)) / np.sum(np.power((experience + self.k), self.alpha))
 
         # Equation 3.1 ends
         pi_qi_flux = options_probability * self.utility_of_choices * self.number_of_agents
 
-        #plotting stuff
+        # plotting stuff
         for i in range(self.options):
             self.orbits[i].append(options_probability[i])
+            self.orbits_for_pi_ei[i].append(options_probability[i] * experience[i])
 
         row_ci = self.discount_rate * experience
 
@@ -87,17 +89,28 @@ class SystemDynamicsWithSdeint:
         soln = sdeint.itoint(self.rate_of_experience, self.noise, self.experiences_of_choices, time_vector)
         #print(self.count)
 
+        ##-------Print the area under the curve over here ---------###
+        for i in range(self.options):
+            print("option " + str(i) + " has area under the curve as:")
+            #print("using composite trapezoidal rule " + '{:18.5f}'.format(trapz(soln[:, i], range(0, len(soln)))))
+            #print("using composite Simpson's rule " + '{:18.5f}'.format(simps(soln[:, i], range(0, len(soln)))))
+
+            print("using composite trapezoidal rule " + '{:18.5f}'.format(trapz(self.orbits_for_pi_ei[i], range(0, len(self.orbits_for_pi_ei[i])))))
+            print("using composite Simpson's rule " + '{:18.5f}'.format(simps(self.orbits_for_pi_ei[i], range(0, len(self.orbits_for_pi_ei[i])))))
+            print("")
+
+
         plt.figure(1)
-        plt.subplot(211)
+        plt.subplot(311)
         for i in range(len(self.orbits)):
             plt.plot(range(len(self.orbits[i])), self.orbits[i], label=("choice " + str(i)))
         #plt.ylim(-1, 1)
-        plt.ylabel('proportion of agents')
+        plt.ylabel('proportion')
         plt.legend(loc='best')
         #plt.legend()
         plt.title('1st: proportion of agents vs time steps -- 2nd: experience of agents vs time steps')
 
-        plt.subplot(212)
+        plt.subplot(312)
         for i in range(self.options):
             plt.plot(time_vector, soln[:, i], label=("choice " + str(i)))
         #plt.rc('lines', linewidth=2.0)
@@ -105,18 +118,26 @@ class SystemDynamicsWithSdeint:
         plt.xlabel('time')
         plt.ylabel('experience')
         #plt.legend(bbox_to_anchor=(1.1, 1.05))
-        plt.legend(loc='best')
+        #plt.legend(loc='best')
         #plt.legend()
+
+        plt.subplot(313)
+        for i in range(self.options):
+            plt.plot(range(len(self.orbits_for_pi_ei[i])), self.orbits_for_pi_ei[i], label=("choice " + str(i)))
+        plt.xlabel('time')
+        plt.ylabel('Pi * Ei')
+        # plt.legend(bbox_to_anchor=(1.1, 1.05))
+        #plt.legend(loc='best')
         plt.show()
 
 
 def main():
-    sysd = SystemDynamicsWithSdeint(number_of_agents= 10, k = 6, alpha = 2,
-                                    utility_of_choices= [1, 1, 1, 1, 1],
-                                    initial_experiences= [0.10001, 0.10001, 0.10001, 0.10002, 0.10001],
-                                    discount_rate=1/1500, sd = 0, rotation_time = 2000, flag = False)
+    sysd = SystemDynamicsWithSdeint(number_of_agents= 10, k = 1, alpha = 2,
+                                    utility_of_choices= [1, 0.9, 0.9, 0.9, 0.9],
+                                    initial_experiences= [0.08, 0.11, 0.12, 0.10, 0.09],
+                                    discount_rate=0.01, sd = 0, rotation_time = 2000, flag = False)
 
-    sysd.solve(time_vector=np.linspace(0, 2000, 1000))
+    sysd.solve(time_vector=np.linspace(0, 50, 25))
 
 
 if __name__ == "__main__":
